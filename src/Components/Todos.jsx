9945c8fb4a9188a91cd4b9b399cd45ef8d1fcdae
Rudra Pratap Singh <30315987+Angryl@users.noreply.github.com>
2021-06-05 01:26:09 +0530

    Update Todos.jsx
    adding the function to display the msg " No Todos To Display" when no item is in the list.

--- src/Components/Todos.jsx
+++ src/Components/Todos.jsx
@@ -10,7 +10,8 @@ export const Todos = (props) => {
         <div className="container" style={myStyle}>
             <h3 className="text-center my-3">Todos List</h3>
                   
-            {props.todos.map((todo)=>{
+            {props.todos.length===0? "No Todos To Display":
+            props.todos.map((todo)=>{
                  return (
                  <>
                  <Todoitems todo={todo} key={todo.sno} onDelete={props.onDelete}/>
